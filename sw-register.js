ec44cf5bc15b87e91aeb29754245e2c32ddc7389
Ruben Dewitte <ruben.dewitte@student.howest.be>
2018-05-24 01:26:35 +0200

    Init sw and manifest

---
+++ sw-register.js
@@ -0,0 +1,7 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register("./sw/sw.js", {scope: "./"}).then(function(registration) {
+        console.log("Service worker registered!");
+    }).catch(function(err) {
+        console.log("Service worker failed to register", err);
+    })
+}

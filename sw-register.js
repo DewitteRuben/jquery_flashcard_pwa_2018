29c8945dce05f0b72240bd3bea9bbda73c0e7e3a
Ruben Dewitte <ruben.dewitte@student.howest.be>
2018-05-24 01:33:45 +0200

    Fixed error with sw

--- sw-register.js
+++ sw-register.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register("./sw/sw.js", {scope: "./"}).then(function(registration) {
+    navigator.serviceWorker.register("./sw.js", {scope: "./"}).then(function(registration) {
         console.log("Service worker registered!");
     }).catch(function(err) {
         console.log("Service worker failed to register", err);

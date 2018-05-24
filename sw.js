714d1e86073fb576058ed9118b7fc6c987faf57b
Ruben Dewitte <ruben.dewitte@student.howest.be>
2018-05-24 09:19:39 +0200

    Testing sw

--- sw.js
+++ sw.js
@@ -1,119 +1,55 @@
-var cacheName = "v1";
-
-var cacheFiles = [
-    "./",
-    "./index.html",
-    "./addcard.html",
-    "./addcardset.html",
-    "./cardgame.html",
-    "./css/screen.css",
-    "./css/materialize.css",
-    "./css/font-awesome.min.css",
-    "./js/addcard.js",
-    "./js/addcardset.js",
-    "./js/DataModule.js",
-    "./js/DomainModule.js",
-    "./js/gamePage.js",
-    "./js/index.js",
-    "./js/jquery-3.3.1.min.js",
-    "./js/localforage.min.js",
-    "./js/materialize.min.js",
-    "./js/materializeinit.js",
-    "./js/translate.js",
-    "./js/utilities.js"
-];
-
-
-self.addEventListener('install', function(e) {
-    console.log('[ServiceWorker] Installed');
-
-    // e.waitUntil Delays the event until the Promise is resolved
-    e.waitUntil(
-
-        // Open the cache
-        caches.open(cacheName).then(function(cache) {
-
-            // Add all the default files to the cache
-            console.log('[ServiceWorker] Caching cacheFiles');
-            return cache.addAll(cacheFiles);
-        })
-    ); // end e.waitUntil
-});
-
-
-self.addEventListener('activate', function(e) {
-    console.log('[ServiceWorker] Activated');
-
-    e.waitUntil(
-
-        // Get all the cache keys (cacheName)
-        caches.keys().then(function(cacheNames) {
-            return Promise.all(cacheNames.map(function(thisCacheName) {
-
-                // If a cached item is saved under a previous cacheName
-                if (thisCacheName !== cacheName) {
-
-                    // Delete that cached file
-                    console.log('[ServiceWorker] Removing Cached Files from Cache - ', thisCacheName);
-                    return caches.delete(thisCacheName);
-                }
-            }));
-        })
-    ); // end e.waitUntil
-
-});
-
-
-self.addEventListener('fetch', function(e) {
-    console.log('[ServiceWorker] Fetch', e.request.url);
-
-    // e.respondWidth Responds to the fetch event
-    e.respondWith(
-
-        // Check in cache for the request being made
-        caches.match(e.request)
-
-
-            .then(function(response) {
-
-                // If the request is in the cache
-                if ( response ) {
-                    console.log("[ServiceWorker] Found in Cache", e.request.url, response);
-                    // Return the cached version
-                    return response;
-                }
-
-                // If the request is NOT in the cache, fetch and cache
-
-                var requestClone = e.request.clone();
-                fetch(requestClone)
-                    .then(function(response) {
-
-                        if ( !response ) {
-                            console.log("[ServiceWorker] No response from fetch ")
-                            return response;
-                        }
-
-                        var responseClone = response.clone();
-
-                        //  Open the cache
-                        caches.open(cacheName).then(function(cache) {
-
-                            // Put the fetched response in the cache
-                            cache.put(e.request, responseClone);
-                            console.log('[ServiceWorker] New Data Cached', e.request.url);
-
-                            // Return the response
-                            return response;
-
-                        }); // end caches.open
-
-                    })
-                    .catch(function(err) {
-                        console.log('[ServiceWorker] Error Fetching & Caching New Data', err);
-                    });
-
-
-            }) // end caches.match(e.request)
-    ); // end e.respondWith
-});
+// var cacheName = "v1";
+//
+// var cacheFiles = [
+//     "/index.html",
+//     "/addcard.html",
+//     "/addcardset.html",
+//     "/cardgame.html",
+//     "/css/screen.css",
+//     "/css/materialize.css",
+//     "/css/font-awesome.min.css",
+//     "/js/addcard.js",
+//     "/js/addcardset.js",
+//     "/js/DataModule.js",
+//     "/js/DomainModule.js",
+//     "/js/gamePage.js",
+//     "/js/index.js",
+//     "/js/jquery-3.3.1.min.js",
+//     "/js/localforage.min.js",
+//     "/js/materialize.min.js",
+//     "/js/materializeinit.js",
+//     "/js/translate.js",
+//     "/js/utilities.js"
+// ];
+//
+// self.addEventListener('install', function(event) {
+//     event.waitUntil(
+//         caches.open(cacheName).then(function(cache) {
+//             return cache.addAll(cacheFiles);
+//         })
+//     );
+// });
+//
+// self.addEventListener('fetch', event => {
+//     // Let the browser do its default thing
+//     // for non-GET requests.
+//     if (event.request.method !== 'GET') return;
+//
+//     // Prevent the default, and handle the request ourselves.
+//     event.respondWith(async function() {
+//         // Try to get the response from a cache.
+//         const cache = await caches.open(cacheName);
+//         const cachedResponse = await cache.match(event.request);
+//
+//         if (cachedResponse) {
+//             // If we found a match in the cache, return it, but also
+//             // update the entry in the cache in the background.
+//             event.waitUntil(cache.add(event.request));
+//             return cachedResponse;
+//         }
+//
+//         // If we didn't find a match in the cache, use the network.
+//         return fetch(event.request);
+//     }());
+// });
+//

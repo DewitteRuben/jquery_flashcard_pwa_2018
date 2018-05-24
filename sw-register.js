17c360395655227afc2aeca228058d2bef616af8
Ruben Dewitte <ruben.dewitte@student.howest.be>
2018-05-24 11:48:36 +0200

    Testing sw

--- sw-register.js
+++ sw-register.js
@@ -1,7 +1,7 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register("./sw.js").then(function(registration) {
+    navigator.serviceWorker.register("./sw.js", {scope: "/2018_SSE_Dewitte_Ruben/"}).then(function (registration) {
         console.log("Service worker registered!");
-    }).catch(function(err) {
+    }).catch(function (err) {
         console.log("Service worker failed to register", err);
     })
 }

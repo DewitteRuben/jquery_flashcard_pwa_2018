68ee6782428ea578e7d65ce9f8518070aeca6131
Ruben Dewitte <ruben.dewitte@student.howest.be>
2018-05-28 07:23:27 +0200

    test

--- js/translate.js
+++ js/translate.js
@@ -3,7 +3,7 @@
 let TranslateModule = function () {
     let API_KEY = "trnsl.1.1.20180519T143130Z.2169786538de90eb.23ca89f814d0ef54f6ee93e5b62d6ae557db4856";
 
-    //test
+    //t
     function getAllAvailableLanguages() {
         let URL = `https://translate.yandex.net/api/v1.5/tr.json/getLangs?key=${API_KEY}&ui=nl`;
         return DataModule.postData(URL).then(function (result) {
